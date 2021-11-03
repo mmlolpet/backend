50c446f28eb7faf2b69edf7993949464aafea02a
Matteus <tolfx@tolfix.com>
2021-11-03 15:32:58 +0100

    New < API/Responses >

---
+++ src/API/Responses/Response.ts
@@ -0,0 +1,22 @@
+import { Response } from "express";
+import Logger from "../../Lib/Logger";
+
+export function APISuccess(msg: any, status: number = 200)
+{
+    // Logger.debug(msg);
+    return (res: Response) => {
+        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+        res.setHeader("X-Total-Count", msg?.length ?? 0)
+        res.status(status).json(msg);
+    }
+}
+
+export function APIError(msg: any, status: number = 400)
+{
+    // Logger.debug(msg);
+    return (res: Response) => {
+        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count')
+        res.setHeader("X-Total-Count", msg?.length ?? 0)
+        res.status(status).json(msg);
+    }
+}
